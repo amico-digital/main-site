4de7ce4248d2b2385bd7ae05a8f39a99065ec001
Ievgen <ievgen@gudyma.com>
2021-05-19 13:19:48 +0300

    Смена hugo на еxtended версию Добавлен CDN для Bootstrap JQuery Смена социальных сетей на месенджеры Удаление themify, magnific popup, slick На страницу барьера добавлено слайд-шоу

--- themes/enov-hugo/assets/js/script.js
+++ themes/enov-hugo/assets/js/script.js
@@ -12,15 +12,7 @@
     once: true
   });
 
-  //   magnific popup video
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-zoom-in',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: true
-  });
+  //   magnific popup video  
 
   $(window).scroll(function () {
 
@@ -52,70 +44,11 @@
   });
 
 
-  $('.portfolio-single-slider').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000
-
-  });
-
-  $('.clients-logo').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000
-  });
-
-  $('.testimonial-wrap').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    dots: false,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 6000,
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }, {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-
-    ]
-  });
+ 
 
 
   $('.portfolio-gallery').each(function () {
-    $(this).find('.popup-gallery').magnificPopup({
-      type: 'image',
-      gallery: {
-        enabled: true
-      }
-    });
+    
   });
 
   // Shuffle js filter and masonry
